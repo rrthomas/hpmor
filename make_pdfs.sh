8923b51a3d7fc07e23628030a416f1d65ca64994
Torben <59419684+entorb@users.noreply.github.com>
2022-05-04 07:39:43 +0200

    auto make via github actions

--- make_pdfs.sh
+++ make_pdfs.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+latexmk hpmor
+
 # TODO
-# latexmk hpmor
 latexmk hpmor-1
-# latexmk hpmor-2
-# latexmk hpmor-3
-# latexmk hpmor-4
-# latexmk hpmor-5
-# latexmk hpmor-6
+latexmk hpmor-2
+latexmk hpmor-3
+latexmk hpmor-4
+latexmk hpmor-5
+latexmk hpmor-6
